00f21e4573167743ac21524d715dd7f5809d0018
dboyce77 <dboyce77@gmail.com>
2018-11-12 19:20:55 -0600

    intial build

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Chicken', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', false);
